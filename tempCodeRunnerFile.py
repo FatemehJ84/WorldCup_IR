57fa74d6f77dfd1d274f1c3160ed2b55e3a9508e
fatemeh.j <fatemehjafarzadeh614@gmail.com>
2026-07-05 22:59:25 +0330

    cleaning

--- tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-engine = SearchEngine(index, processed_docs,builder.df)
-# controller = Controller(engine, tfidf)
-
-# while True:
-#     query = input("\nSearch : ")
-#     if query.lower()=="exit":
-#         print("Good Bye!")
-#         break
-#     controller.choose(query)
